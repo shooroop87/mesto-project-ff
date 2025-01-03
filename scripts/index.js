885372931004e3f09754cb7df09a8f7decb78f0f
Andrey <shooroop87@mail.ru>
2025-01-04 01:05:05 +0300

    v1
    v1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardData, handleDeleteCard) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  deleteButton.addEventListener('click', () => {
+    handleDeleteCard(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function handleDeleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+    const card = createCard(cardData, handleDeleteCard);
+    placesList.append(card);
+});
