7f31cdd991b2d9ccf3c30777e0d1a10e3633a07a
Andrey <shooroop87@mail.ru>
2025-01-04 01:59:36 +0300

    v2
    v2

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,10 @@ const cardTemplate = document.querySelector('#card-template').content.querySelec
 
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
+const addButton = document.querySelector('.profile__add-button');
+const newCardPopup = document.querySelector('.popup_type_new-card');
+const newCardForm = newCardPopup.querySelector('.popup__form');
+const closePopupButton = newCardPopup.querySelector('.popup__close');
 
 // @todo: Функция создания карточки
 function createCard(cardData, handleDeleteCard) {
@@ -27,8 +31,42 @@ function handleDeleteCard(cardElement) {
   cardElement.remove();
 }
 
+// @todo: Функция открытия попапа
+function openPopup(popup) {
+  popup.classList.add('popup_is-opened');
+}
+
+// @todo: Функция закрытия попапа
+function closePopup(popup) {
+  popup.classList.remove('popup_is-opened');
+}
+
+// @todo: Обработчик добавления новой карточки
+function handleNewCardSubmit(event) {
+  event.preventDefault();
+
+  const placeName = newCardForm.querySelector('.popup__input_type_card-name').value;
+  const placeLink = newCardForm.querySelector('.popup__input_type_url').value;
+
+  const newCardData = {
+    name: placeName,
+    link: placeLink,
+  };
+
+  const newCard = createCard(newCardData, handleDeleteCard);
+  placesList.append(newCard);
+
+  closePopup(newCardPopup);
+  newCardForm.reset();
+}
+
+// @todo: Слушатели событий
+addButton.addEventListener('click', () => openPopup(newCardPopup));
+closePopupButton.addEventListener('click', () => closePopup(newCardPopup));
+newCardForm.addEventListener('submit', handleNewCardSubmit);
+
 // @todo: Вывести карточки на страницу
 initialCards.forEach((cardData) => {
-    const card = createCard(cardData, handleDeleteCard);
-    placesList.append(card);
+  const card = createCard(cardData, handleDeleteCard);
+  placesList.append(card);
 });
